c341e3a068b8b82a853761d0318736e73bdfe726
Rahul <bathijarahul@gmail.com>
2024-05-25 22:44:13 -0700

    added footer without styling

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <Link href="/" legacyBehavior>
+          <a className="logo">YT Creator Tools</a>
+        </Link>
+        <ul className="footer-links">
+          <li>
+            <Link href="/about" legacyBehavior>
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/pricing" legacyBehavior>
+              <a>Pricing</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" legacyBehavior>
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
