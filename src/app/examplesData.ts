a5ae32164cced28e454140734d7fd9e9a19696d6
Rahul <bathijarahul@gmail.com>
2024-05-26 15:15:48 -0700

    more changes

--- src/app/examplesData.ts
+++ src/app/examplesData.ts
@@ -3,18 +3,6 @@ const examplesData = [
     { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
     { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
     { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample.png', description: 'Example 2 description' },
-    { id: 2, src: './youtubethumbnailexample2.png', description: 'Example 2 description' },
     // Add more examples as needed
   ];
   
