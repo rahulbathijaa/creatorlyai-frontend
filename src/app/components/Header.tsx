a672518e06e978782971ea5cd95fd28590528eef
Rahul <bathijarahul@gmail.com>
2024-05-26 14:35:21 -0700

    mostly complete header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -22,13 +22,15 @@ const Header: React.FC = () => {
     <div className="header-container">
       <h1 className="main-title">A Modern Way to Optimize YouTube Content</h1>
       <p className="subtitle">Generate YouTube thumbnails and descriptions in seconds</p>
-      <button className="signup-button">Sign up</button>
-      <button className="upload-button">Try uploading your own video</button>
+      <div className="button-group">
+        <button className="signup-button">Try it out</button>
+        <button className="upload-button">Sign up</button>
+      </div>
       <div
         className="upload-container"
         style={{ backgroundImage: `url(${currentImage})` }}
       >
-        
+
       </div>
     </div>
   );
